f29d8e0df7c2d25212f6ab3e8feac71d768b4166
DKNight-9207 <yuchen920707@gmail.com>
2025-07-24 12:53:05 +0800

    bug fix

--- piscesORM/__init__.py
+++ piscesORM/__init__.py
@@ -1,9 +1,11 @@
 from . import column
 from . import table
+from . import operator
+from . import generator
 from . import engine
 from . import errors
 from . import lock
 from . import session
 
-__version__ = "0.0.alpha1"
+__version__ = "0.0.1"
 
